2eeb5053c54d37eb06f559d01a84e39b6dc00128
LiZi <mujiangjuzi@foxmail.com>
2021-04-02 17:42:30 +0800

    指针进阶05
    指针进阶05

--- 指针进阶/指针进阶/zhizhenjinjie.c
+++ 指针进阶/指针进阶/zhizhenjinjie.c
@@ -316,37 +316,111 @@
 //}
 
 
+//
+//int cmp_int(const void* e1, const void* e2)
+//{
+//    return *(int*)e1 - *(int*)e2;
+//}
+//void test()
+//{
+//    int arr[] = { 0 };
+//    int n = 0;
+//    int i = 0;
+//    int a = 0;
+//    printf("排序>:\n");
+//    printf("亲输入要排序的数字个数>:\n");
+//    scanf("%d", &n);
+//    for (i = 0; i < n; i++)
+//    {
+//        printf("请输入数字>:\n");
+//        scanf("%d", &a);
+//        arr[i] = a;
+//    }
+//    qsort(arr, n, sizeof(arr[0]), cmp_int);
+//    printf("正确顺序是:>\n");
+//    for (i = 0; i < n; i++)
+//    {
+//        printf(" %d", arr[i]);
+//    }
+//    printf("\n");
+//    system("pause");
+//}
+//int main()
+//{
+//    test();
+//    return 0;
+//}
 
+void Swap(char* buf1, char* buf2,int width)
+{
+	int i = 0;
+	for ( i = 0; i < width; i++)
+	{
+		char tmp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = tmp;
+		buf1++;
+		buf2++;
+	}
+}
+void bubble_sort(void* base,int sz,int width,int (*cmp)(void* e1,void* e2))
+{
+	int i = 0;
+	//趟数
+	for ( i = 0; i < sz-1; i++)
+	{
+		//每一趟比较的对数
+		int j = 0;
+		for ( j = 0; j < sz-1-i; j++)
+		{
+			//两个元素的比较
+			if (cmp((char*)base+j*width,(char*)base+(j+1)*width)>0)
+			{
+				//交换
+				Swap((char*)base + j * width, (char*)base + (j + 1) * width,width);
+			}
+		}
+	}
+}
+struct stu
+{
+	char name[10];
+	int age;
+};
 int cmp_int(const void* e1, const void* e2)
 {
-    return *(int*)e1 - *(int*)e2;
+	return *(int*)e1 - *(int*)e2;
 }
-void test()
+int cmp_stu_by_age(const void* e1, const void* e2)
 {
-    int arr[] = { 0 };
-    int n = 0;
-    int i = 0;
-    int a = 0;
-    printf("排序>:\n");
-    printf("亲输入要排序的数字个数>:\n");
-    scanf("%d", &n);
-    for (i = 0; i < n; i++)
-    {
-        printf("亲输入数字>:\n");
-        scanf("%d", &a);
-        arr[i] = a;
-    }
-    qsort(arr, n, sizeof(arr[0]), cmp_int);
-    printf("正确顺序是:>\n");
-    for (i = 0; i < n; i++)
-    {
-        printf(" %d", arr[i]);
-    }
-    printf("\n");
-    system("pause");
+	return ((struct stu*)e1)->age - ((struct stu*)e2)->age;
+}
+test4()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+	bubble_sort(arr,sz,sizeof(arr[0]),cmp_int);
+	for ( i = 0; i < sz; i++)
+	{
+		printf("%d", arr[i]);
+	}
+	printf("\n");
+}
+test5()
+{
+	struct stu s[3] = { {"zhangsan",20},{"lisi",40},{"wangwu",10} };
+	int sz = sizeof(s) / sizeof(s[0]);
+	int i = 0;
+	bubble_sort(s, sz,sizeof(s[0]), cmp_stu_by_age);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d%s", s[i].age,s[i].name);
+	}
 }
 int main()
 {
-    test();
-    return 0;
+	test4();
+	test5();
+	return 0;
 }
