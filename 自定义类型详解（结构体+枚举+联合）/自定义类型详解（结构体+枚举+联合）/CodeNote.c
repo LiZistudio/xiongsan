3efa8e47dbd9a95699a8eb22e8c2b36e9e2d1845
LiZi <mujiangjuzi@foxmail.com>
2021-04-17 21:39:35 +0800

    自定义类型详解（结构体+枚举+联合）4.17
    自定义类型详解（结构体+枚举+联合）4.17

--- 自定义类型详解（结构体+枚举+联合）/自定义类型详解（结构体+枚举+联合）/CodeNote.c
+++ 自定义类型详解（结构体+枚举+联合）/自定义类型详解（结构体+枚举+联合）/CodeNote.c
@@ -58,17 +58,84 @@ int main()
 //}
 
 
-#include <stddef.h>
-typedef struct s {
-	char c1;//c1的偏移量为0
-	int i;//i的偏移量为4
-	double d;//d的偏移量为8（以上均为相对起始位置）
-}S;
+//#include <stddef.h>
+//typedef struct s {
+//	char c1;//c1的偏移量为0
+//	int i;//i的偏移量为4
+//	double d;//d的偏移量为8（以上均为相对起始位置）
+//}S;
+//int main()
+//{
+//	//offsetof();//计算结构体成员相对于起始位置的偏移量 宏
+//	printf("%d\n", offsetof(struct s, c1));
+//	printf("%d\n", offsetof(struct s, i));
+//	printf("%d\n", offsetof(S, d));
+//	return 0;
+//}
+
+//*******************************************************结构体传参***********************************************************
+
+//struct s {
+//	char name[10];
+//	int age;
+//	double tmp;
+//};
+//void Init(struct s* ps)
+//{
+//	ps->age = 20;
+//	ps->name[9] = "lizi";
+//	ps->tmp = 37.5;
+//}
+//int main()
+//{
+//	//struct s stu = { "lizi",20,37.5 };
+//	struct s stu;
+//	Init(&stu);
+//	/*stu.name[10] = "lizi";
+//	stu.age = 20;
+//	stu.tmp = 37.5;
+//
+//	printf("%d\n", stu.age);*/
+//	printf("%d\n", stu.age);
+//	return 0;
+//}
+//*******************************位段********************************
+//struct S
+//{
+//	int _a : 2;
+//	int _b : 5;
+//	int _c : 10;
+//	int _d : 30;
+//};
+//int main()
+//{
+//	struct S s;
+//	printf("%d\n", sizeof(s));
+//	
+//	return 0;
+//}
+
+//****************************枚举************************************
+
+
+enum Sex//枚举类型
+{
+	//枚举的可能取值
+	MALE,//0
+	FEMALE,//1
+	SECRET//2
+};
+enum color
+{
+	red,//0
+	yellow,//1
+	green,//2
+	blue//3
+};
 int main()
 {
-	//offsetof();//计算结构体成员相对于起始位置的偏移量 宏
-	printf("%d\n", offsetof(struct s, c1));
-	printf("%d\n", offsetof(struct s, i));
-	printf("%d\n", offsetof(struct s, d));
+	enum Sex s = MALE;
+	enum color c = green;
+
 	return 0;
 }
