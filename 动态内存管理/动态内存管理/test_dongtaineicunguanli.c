90a31394152b73b7568cc654a59ab803e745cf61
LiZi <mujiangjuzi@foxmail.com>
2021-04-22 17:37:03 +0800

    动态内存管理v1.4.22
    动态内存管理v1.4.22

---
+++ 动态内存管理/动态内存管理/test_dongtaineicunguanli.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//	____                      ____
+///	|	|                  / |    |
+//|	|	|                  | |____|
+//|	|	|                  |/____/
+//|	|	|                   ______
+//|	|	|                 / |     |  
+//|	|	|______________   | |     |  
+//|	|__________________|  | |     |
+//|/___________________/  | |_____|
+//                        | /_____/	
+//
+
+#include <stdio.h>
+
+//struct Stu
+//{
+//	char name[10];
+//	int age;
+//};
+//int main()
+//{
+//	struct Stu s[30];//存放30个struct Stu类型的数据
+//	//20浪费
+//	//50不够
+//	//所以需要动态内存分配
+//
+//	return 0;
+//}
+
+//c语言是可以创建变长数组——c99中增加了
+//int n=0;
+//struct Stu s[n];
+//vs中不支持
+//#include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+int main()
+{
+	//向内存申请10个整型的空间
+	//int* p = (int*)malloc(INT_MAX);//Not enough space
+	int* p = (int*)malloc(10 * sizeof(int));
+	if (p==NULL)
+	{
+		//打印错误原因的一个方式
+		printf("%s\n", strerror(errno));
+	}
+	else
+	{
+		//正常使用空间
+		int i = 0;
+		for ( i = 0; i < 10; i++)
+		{
+			*(p + i) = i;
+		}
+		for (i = 0; i < 10; i++)
+		{
+			printf("%d ", *(p + i));
+		}
+	}
+	//当动态申请的空间不再使用时应该还给操作系统
+	free(p);//主动回收
+	p = NULL;//free完之后的p仍然能访问原来的地址，但p已经被断开，所指向的内存空间被释放——野指针，所以将其置空是最安全的做法
+
+	return 0;
+}
