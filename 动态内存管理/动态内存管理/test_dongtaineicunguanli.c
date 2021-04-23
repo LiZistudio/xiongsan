02b6bcfadcc3c1ac8e488c267ac00393d012f110
LiZi <mujiangjuzi@foxmail.com>
2021-04-23 22:12:21 +0800

    动态内存管理v3.4.23
    动态内存管理v3.4.23

--- 动态内存管理/动态内存管理/test_dongtaineicunguanli.c
+++ 动态内存管理/动态内存管理/test_dongtaineicunguanli.c
@@ -37,32 +37,103 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-int main()
-{
-	//向内存申请10个整型的空间
-	//int* p = (int*)malloc(INT_MAX);//Not enough space
-	int* p = (int*)malloc(10 * sizeof(int));
-	if (p==NULL)
-	{
-		//打印错误原因的一个方式
-		printf("%s\n", strerror(errno));
-	}
-	else
-	{
-		//正常使用空间
-		int i = 0;
-		for ( i = 0; i < 10; i++)
-		{
-			*(p + i) = i;
-		}
-		for (i = 0; i < 10; i++)
-		{
-			printf("%d ", *(p + i));
-		}
-	}
-	//当动态申请的空间不再使用时应该还给操作系统
-	free(p);//主动回收
-	p = NULL;//free完之后的p仍然能访问原来的地址，但p已经被断开，所指向的内存空间被释放——野指针，所以将其置空是最安全的做法
+//int main()
+//{
+//	//向内存申请10个整型的空间
+//	//int* p = (int*)malloc(INT_MAX);//Not enough space
+//	int* p = (int*)malloc(10 * sizeof(int));
+//	if (p==NULL)
+//	{
+//		//打印错误原因的一个方式
+//		printf("%s\n", strerror(errno));
+//	}
+//	else
+//	{
+//		//正常使用空间
+//		int i = 0;
+//		for ( i = 0; i < 10; i++)
+//		{
+//			*(p + i) = i;
+//		}
+//		for (i = 0; i < 10; i++)
+//		{
+//			printf("%d ", *(p + i));
+//		}
+//	}
+//	//当动态申请的空间不再使用时应该还给操作系统
+//	free(p);//主动回收
+//	p = NULL;//free完之后的p仍然能访问原来的地址，但p已经被断开，所指向的内存空间被释放——野指针，所以将其置空是最安全的做法
+//
+//	return 0;
+//}
+//****************************calloc*********************
+//int main()
+//{
+//	int* p = (int*)calloc(10, sizeof(int));
+//	if (p==NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//	}
+//	else
+//	{
+//		int i = 0;
+//		for ( i = 0; i < 10; i++)
+//		{
+//			printf("%d ",*(p + i));
+//		}
+//	}
+//	//释放空间
+//	free(p);
+// p=NULL;
+//	return 0;
+//}
+//*************************realloc**************************
+//int main()
+//{
+//	int* p = (int*)malloc(20);
+//	if (p==NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//	}
+//	else
+//	{
+//		int i = 0;
+//		for ( i = 0; i < 5; i++)
+//		{
+//			*(p + i) = i;
+//		}
+//	}
+//	//只是在使用malloc开辟的20个字节的空间
+//	//假设到这里20个字节不能满足使用了
+//	//我们希望能够有40个字节的空间
+//	//这里就可以用realloc来调整动态开辟的内存了
+//	int* rp = (int*)realloc(p, 40);
+//	int i = 0;
+//	for ( i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(rp + i));
+//	}
+//	free(rp);
+//	rp = NULL;
+//	return 0;
+//}
 
-	return 0;
-}
+
+//int main()
+//{
+//	int* p = (int*)malloc(40);
+//	//万一malloc失败了，p就被赋值为NULL。
+//	//一定要先进行安全检查
+//	if (p==NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//	}
+//	int i = 0;
+//	for ( i = 0; i < 10; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
