def99f288d6a278aa8d24af6d653a93e4fae6dd9
LiZi <mujiangjuzi@foxmail.com>
2021-05-04 23:27:59 +0800

    程序环境和预处理v1.5.4
    程序环境和预处理v1.5.4

---
+++ 程序环境和预处理/程序环境和预处理/test.c
@@ -0,0 +1,96 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//	____                      ____
+///	|	|                  / |    |
+//|	|	|                  | |____|
+//|	|	|                  |/____/
+//|	|	|                   ______
+//|	|	|                 / |     |  
+//|	|	|______________   | |     |  
+//|	|__________________|  | |     |
+//|/___________________/  | |_____|
+//                        | /_____/	
+//
+
+
+#include<stdio.h>
+//int g_val = 1000;
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	for ( i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	for ( i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%d\n", __STDC__);
+//	return 0;
+//}
+
+//#define MAX 100
+//#define STR "hehe"
+//int main()
+//{
+//	printf("%s\n", STR);
+//	return 0;
+//}
+
+//ºê
+
+
+//#define SQUARE(X) X*X
+//int main()
+//{
+//	int ret = SQUARE(5);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//void print(int a)
+//{
+//	printf("the value of a is %d\n", a);
+//
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	//printf("the value of a is %d\n", a);
+//	print(a);
+//	print(b);
+//	return 0;
+//}
+
+
+//****************#ºÍ##**************
+//#define PRINT(X) printf("The vlaue of "#X" is %d\n",X)
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	PRINT(a);
+//	PRINT(b);
+//	return 0;
+//}
+
+//*******##*************
+//#define CAT(X,Y) X##Y
+//int main()
+//{
+//	int Class84 = 2019;
+//	printf("%d\n", CAT(Class, 84));
+//	return 0;
+//}
+
